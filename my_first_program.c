958915d74fe62293a8a725e28927709ef81a9726
preronaain99 <preronaain7459@gmail.com>
2018-08-26 13:01:02 +0530

    My first commit ever

---
+++ my_first_program.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Please adjust tab space to 8 as per linux kernel indentation guidelines");
+    printf("Then write hello world..haha!");
+    return 0;
+}
