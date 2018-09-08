22634c7993fe75c4e1c4e217a2df0d1b4678b448
preronaain99 <preronaain7459@gmail.com>
2018-09-08 18:08:14 +0530

    Prime factors of a given number

---
+++ prime.c
@@ -0,0 +1,37 @@
+ #include<stdio.h>
+
+ void main()
+ {
+	 int n, i ;
+	 printf("enter a number :");
+	 scanf("%d", &n);
+	 for(i = 2; n > 1; i++){
+		 while(n % i == 0){
+			 n = n / i;
+			 printf("%d ", i);
+		 }
+	 }
+
+ }
+ 
+
+     
+      
+         
+         
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
