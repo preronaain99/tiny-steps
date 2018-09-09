ce28bbf9512fea8c39540b955a20b0e78961a238
preronaain99 <preronaain7459@gmail.com>
2018-09-09 14:37:06 +0530

    Primefactors

--- prime.c
+++ prime.c
@@ -2,15 +2,9 @@
 
  void main()
  {
-	int n, i ;
-	printf("enter a number :");
-	scanf("%d", &n);
-	for(i = 2; n > 1; i++){
-		while(n % i == 0){
-			n = n / i;
-			printf("%d ", i);
-		}
-	}
+	 int num[5] = {12,34,54,66,11};
+	 
+
  }
  
 
---
+++ primefactors.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+
+void main()
+{
+        int num[5] = {12,34,54,66,11};
+	int i, j = 2, k;
+	int num[k];
+	num[k] = num[5];
+	for(i = 0; i < 5; i++){
+		while(i % j == 0 ){
+			i = i / j;
+			int primefactors(int k);
+			printf("%d\n", k);
+
+		}
+
+		
+
+		
+	}
+}
