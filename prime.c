69f7fa08d8eb947ba06799f20ccbc0698b01c1e8
preronaain99 <preronaain7459@gmail.com>
2018-09-17 19:38:28 +0530

    Extended string compression program

--- prime.c
+++ prime.c
@@ -2,7 +2,18 @@
 
  void main()
  {
-	 int num[5] = {12,34,54,66,11};
+	 int lower, upper, n, i = 2;
+	 printf("lower range:");
+	 scanf("%d", &lower);
+	 printf("upper range:");
+	 scanf("%d", &upper);
+	 for(n = lower+1; n < upper; n++){
+		 while(n % i == 1){
+			 printf("%d", n);
+
+		 }
+
+	 }
 	 
 
  }
--- primefactors.c
+++ primefactors.c
@@ -1,33 +1,39 @@
-#include<stdio.h>
+ #include<stdio.h>
+ #include<string.h>
 
-void primefactors(int m)
+int main()
 {
-	int i;
+	int i, m, p, count = 0;
+        scanf("%d", &m);
 	for(i = 2; m > 1; i++){
 		while(m % i == 0){
 			m = m / i;
 			printf("%d ", i);
+                        count++;
+                        p = i;
+                        int storefac[count++] = p + 48;
 		}
 	}
-}
 
-void main()
-{
-	int i, m, arr[50];
-	scanf("%d", &m);
-	for(i = 0; i < m; i++){
-		scanf("%d", &arr[i]);
-	}
-
-
-	//bad practice sorry//dint say sorry when you dont know dynamic allocation
-	for(i = 0; i < m; i++){
-		/*call prime factor function*/
-		primefactors(arr[i]);
-		printf("\n");
-	}
-}
 	
+        int j = 0, sum = 1, ch = 0;
+        char store_char[50];
+        /*I CANNOT UNDERSTAND HOW TO PROCEED AFTER THIS*/
+        for( j = 0; j < len; i++ ) {
+                if( storefac[j] == storefac[j + 1] ) {
+                        sum++;
+                } else {
+                        store_char[ch] = sum + 48;
+                        store_char[ch + 1] = storefac[j];
+                        ch += 2;        
+                        sum = 1;
+                }
+        }        
+        store_char[ch] = '\0';
+        printf("%s\n", store_char);
+        return 0;
+}
+
 
 
 
