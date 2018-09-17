0461ea6e5b407571a18e7f7a9822a053a6bc5d79
preronaain99 <preronaain7459@gmail.com>
2018-09-18 00:17:25 +0530

    Changes made

--- primefactors.c
+++ primefactors.c
@@ -3,34 +3,32 @@
 
 int main()
 {
-	int i, m, p, count = 0;
+	int i, m, count = 0;
         scanf("%d", &m);
 	for(i = 2; m > 1; i++){
 		while(m % i == 0){
 			m = m / i;
 			printf("%d ", i);
-                        count++;
-                        p = i;
-                        int storefac[count++] = p + 48;
+                        count++ ;
 		}
 	}
 
 	
-        int j = 0, sum = 1, ch = 0;
-        char store_char[50];
-        /*I CANNOT UNDERSTAND HOW TO PROCEED AFTER THIS*/
-        for( j = 0; j < len; i++ ) {
-                if( storefac[j] == storefac[j + 1] ) {
+        int j = 0, sum = 0;
+        int store_fac[count++];
+        int store_char[50];
+        for( j = 0; j < count; j++ ) {
+                if( store_fac[j] == store_fac[j + 1] ) {
                         sum++;
                 } else {
-                        store_char[ch] = sum + 48;
-                        store_char[ch + 1] = storefac[j];
-                        ch += 2;        
+                        store_char[j] = i + 48;
+                        store_char[j + 1] = store_fac[j];
+                        j += 2;        
                         sum = 1;
                 }
         }        
-        store_char[ch] = '\0';
-        printf("%s\n", store_char);
+        store_char[j] = (count) - 1;
+        printf("%d\n", store_char);
         return 0;
 }
 
