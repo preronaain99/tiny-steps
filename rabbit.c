c1dfab996f9ab4a44f1313fbd522bf650b6866d1
preronaain99 <preronaain7459@gmail.com>
2018-09-19 16:03:21 +0530

    Modifications in rabbit

--- rabbit.c
+++ rabbit.c
@@ -2,26 +2,24 @@
 
 int main()
 {
-        int k, carrot = 1, barren = 0;
+        int k, carrot = 0, barren = 0;
 	int dead = 0, alive = 1;
 	scanf("%d", &k);
 	int arr[k], a = 5;
-	int hop = k / 2;
-	while(k >=20 && hop >= 10){
-		for(int i = 0; i < k; i++){
-			scanf("%d", &arr[i]);
-			if(arr[i] = carrot){
-			        carrot++;
-			}
-		        else if(arr[i] = barren){
-			        barren++;
-			}
-		        else if (arr[i] != 1 || arr[i] !=0){
-			        printf("%d", a );
-			}
+	for(int i = 0; i < k; i++){
+		scanf("%d", &arr[i]);
+		if(arr[i] == 1){
+			carrot++;
 		}
-		
+		else if(arr[i] == 0){
+		        barren++;
+		}
+		else if (arr[i] != 1 || arr[i] !=0){
+		        printf("%d", a );
+		}
 	}
+		
+	
 	if((carrot++) >= 8){
 		printf("%d", alive);
 	}
