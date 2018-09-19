a776cca2f9d5024d732582a76db895dc4a14f0dd
ricoseeds <rghchaks73@gmail.com>
2018-09-19 22:52:56 +0530

    Validation fix

--- refactor.c
+++ refactor.c
@@ -7,23 +7,33 @@ int main()
         int valid = 1;
 	scanf("%d", &k);
 	int arr[k], a = 5;
+        // input
 	for(int i = 0; i < k; i++){
 		scanf("%d", &arr[i]);
-                if(arr[i] != 1 && arr[i] != 0){
+        }
+        // validate
+        if( k < 20){
+                valid = 0;
+        }
+        for(int i = 0; i < k; i++){
+                if(( arr[i] != 1 && arr[i] != 0 )){
                         valid = 0;
                         break;
                 }
         }
-        printf("%d", a);
-        if(valid == 1){
-                for(int i = 0; i < k; i+=2){
-                        if(arr[i] == 1){
-			carrot++;
-		}
+        if(!valid){
+                printf("%d", a);
         }
-                if((carrot) >= 8){
+        // rest of the code 
+        else if(valid == 1){ 
+                for(int i = 0; i < k; i += 2){
+                        if(arr[i] == 1){
+		                carrot++;
+		        }
+                }
+                if(carrot >= 8){
                         printf("%d", alive);
-                } else{
+                } else {
                         printf("%d", dead);
                 }
                 
