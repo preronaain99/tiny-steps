479ea7ca8a5b2d7a1982c4869bdf5f536d0370b4
preronaain99 <preronaain7459@gmail.com>
2018-11-03 10:16:11 +0530

    XOR cipher

---
+++ cipher.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+void encryptDecrypt(char inpString[])
+{
+        char xorKey = 'P';
+        int len = strlen(inpString);
+        int i;
+        for(i = 0; i < len; i++){
+                inpString[i] = inpString[i] ^ xorKey;
+                printf("%c", inpString[i]);
+        }
+}
+void main()
+{
+        char sampleString[] = "ArghaChakraborty";
+        printf("Encrypted string: ");
+        encryptDecrypt(sampleString);
+        printf("\n");
+        printf("Decrypted string: ");
+        encryptDecrypt(sampleString);
+
+
+
+}
